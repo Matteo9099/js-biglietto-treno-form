207a93a8769f664259cdf6b6af5761ca4285e0b8
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-24 16:58:34 +0100

    push

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let name = document.getElementById('name');
-let km = document.getElementById('kmDaPercorrere');
-let eta = document.getElementById('eta');
+
 let button = document.getElementById('calcola');
 
-    // costo al km
+   
+button.addEventListener('click',
+    function(){
+
+    let name = document.getElementById('name').value;
+    let km = document.getElementById('kmDaPercorrere').value;
+    let eta = document.getElementById('eta').value;
+
+     // costo al km
     let prezzoAlKm = 0.21;
 
     // prezzo biglietto intero
@@ -17,24 +23,28 @@ let button = document.getElementById('calcola');
     let scontoOver = (prezzoBiglietto * 40) /100;
     let prezzoOver = prezzoBiglietto - scontoOver;
     
-
-button.addEventListener('click',
-    function(){
+    document.getElementById('resultName').innerHTML = name;
 
         if(name.value=="" || km.value=="" || eta.value==""){
             alert('Inserisci un valore valido.');
         }        
-        else if (eta < 18){
+        else if (eta == 0){
             document.getElementById('resultEta').innerHTML = "Under 18";
+            document.getElementById('resultCarrozza').innerHTML = Math.floor(Math.random()*10);
+            document.getElementById('resultCodice').innerHTML = Math.floor(Math.random()*100);
+            document.getElementById('resultPrezzo').innerHTML = prezzoBiglietto + " &euro;";      
         }
-        else if(eta > 65){
+        else if(eta == 2){
             document.getElementById('resultEta').innerHTML = "Over 65";
+            document.getElementById('resultCarrozza').innerHTML = Math.floor(Math.random()*10);
+            document.getElementById('resultCodice').innerHTML = Math.floor(Math.random()*100);
+            document.getElementById('resultPrezzo').innerHTML = prezzoBiglietto + " &euro;";
         }
-        else{
+        else {
             document.getElementById('resultEta').innerHTML = "Prezzo standard";
             document.getElementById('resultCarrozza').innerHTML = Math.floor(Math.random()*10);
             document.getElementById('resultCodice').innerHTML = Math.floor(Math.random()*100);
-            document.getElementById('resultPrezzo').innerHTML = prezzoBiglietto;
+            document.getElementById('resultPrezzo').innerHTML = prezzoBiglietto + " &euro;";
         }
     }
 )
