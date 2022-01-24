f34b534de5c3c50e8d66652250556103e5ff4ac3
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-24 15:33:30 +0100

    creata strutture e inizio js

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = document.getElementById('name');
+let km = document.getElementById('kmDaPercorrere');
+let eta = document.getElementById('eta');
+let button = document.getElementById('calcola');
+
